eb1ebdc37e8709202409fe9ad2a677ac5fb6c79a
unknown <Kat2bk@gmail.com>
2020-03-06 17:39:39 -0800

    added axios call to retrieve data, added useEffect and useState to React

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Container from "./containerChar";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response => {
+        console.log("This is the data", response.data.results);
+      })
+      .catch(error => {
+        console.log("we are unable to retrieve data", error);
+      });
+  }, []);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Container postdata={data} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ starwars/src/containerChar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Container(props) {
+  return (
+    <div>
+      <h3>{props.postdata.name}</h3>
+    </div>
+  );
+}
