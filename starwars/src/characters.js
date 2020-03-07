769a381985e4d3c10f7f82d9602d3952b8d0cc2b
unknown <Kat2bk@gmail.com>
2020-03-06 18:11:17 -0800

    added styled-components, began styling

--- starwars/src/characters.js
+++ starwars/src/characters.js
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  width: 250px;
+  padding: 5px;
+  margin: 10px;
+  border-radius: 10px;
+  border: 1px solid brown;
+`;
 
 export default function Characters(props) {
   return (
-    <div>
-      <h3>{props.data.name}</h3>
-    </div>
+    <Box>
+      <h3>Name: {props.data.name}</h3>
+      <h3>Year of Birth: {props.data.birth_year}</h3>
+      <h3>Gender: {props.data.gender}</h3>
+    </Box>
   );
 }
